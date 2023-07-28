7d3b76ab58ae7dbadbe165336b39aa820847e639
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-07-28 09:33:44 +0200

    commit prova

---
+++ src/main/java/com/example/U5S1ProvaSettimanale/U5S1ProvaSettimanaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.U5S1ProvaSettimanale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S1ProvaSettimanaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S1ProvaSettimanaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U5S1ProvaSettimanale/U5S1ProvaSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U5S1ProvaSettimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S1ProvaSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
